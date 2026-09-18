0b2a0e6c5e404fe6872fea9efa3e6f305a9ef682
NhatAizen <trannguyennhunhat2005@gmail.com>
2026-09-18 17:05:22 +0700

    fix(realtime): restrict WebSocket origins

--- services/realtime-service/src/main/java/com/smarthotel/realtime/config/WebSocketConfig.java
+++ services/realtime-service/src/main/java/com/smarthotel/realtime/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.smarthotel.realtime.config;
 import com.smarthotel.realtime.security.JwtHandshakeInterceptor;
 import com.smarthotel.realtime.websocket.RealtimeWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -13,16 +14,22 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     private final RealtimeWebSocketHandler handler;
     private final JwtHandshakeInterceptor interceptor;
+    private final String[] allowedOriginPatterns;
 
-    public WebSocketConfig(RealtimeWebSocketHandler handler, JwtHandshakeInterceptor interceptor) {
+    public WebSocketConfig(
+            RealtimeWebSocketHandler handler,
+            JwtHandshakeInterceptor interceptor,
+            @Value("${app.realtime.allowed-origin-patterns}") String allowedOriginPatterns
+    ) {
         this.handler = handler;
         this.interceptor = interceptor;
+        this.allowedOriginPatterns = allowedOriginPatterns.split("\\s*,\\s*");
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(handler, "/ws/realtime")
                 .addInterceptors(interceptor)
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns(allowedOriginPatterns);
     }
 }
